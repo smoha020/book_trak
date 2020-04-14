0543fd19e02033d200a73118a32bb6ab02fc648b
Said <smoha020@ucr.edu>
2020-04-14 14:00:07 -0700

    changed ip to all zeros

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/author', require('./routes/author'));
 if(process.env.NODE.ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'front_end', 'build')));
 
-  console.log('inside if')
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
   });
