2a70faa803a55651003ec23d1635e4c7dd73c9bc
Said <smoha020@ucr.edu>
2020-04-14 11:57:37 -0700

    used relative file path instead for express static

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use('/book', require('./routes/book'));
 app.use('/author', require('./routes/author'));
 
 if(process.env.NODE.ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'front_end', 'build')));
+  app.use(express.static('front_end/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
