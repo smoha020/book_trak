514358c907ef059f0ec4628c7e68bde847f1f718
Said <smoha020@ucr.edu>
2020-04-14 21:40:30 -0700

    put a / before *

---
+++ front_end/build/precache-manifest.7bb2d611699836e8d1177555fbd201e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6fb28231f0dd86f54b5ba739434d027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d511f677773c647d51cf",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e07ead700756a02d285d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d511f677773c647d51cf",
+    "url": "/static/js/2.9594da29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9594da29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07ead700756a02d285d",
+    "url": "/static/js/main.4664cf9d.chunk.js"
+  },
+  {
+    "revision": "bc2dde3a8451f2c5b93e",
+    "url": "/static/js/runtime-main.27ac2b94.js"
+  }
+]);
